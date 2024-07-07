555890ec1dc18fdc1d4997b52bdd0f022c3ece09
gh0stintheshe11 <langs.971104@gmail.com>
2024-07-07 07:27:49 -0400

    update

--- src/card/renderStats.js
+++ src/card/renderStats.js
@@ -5,6 +5,38 @@ function renderStats(stats) {
   const svg = `
     <svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
       <style>
+
+        @keyframes reveal-left-to-right {
+          from {
+            clip-path: polygon(0 0, 0% 0, 0% 100%, 0 100%);
+          }
+          to {
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+          }
+        }
+
+        @keyframes reveal-top-to-bottom {
+          from {
+            clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
+          }
+          to {
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+          }
+        }
+        
+        @keyframes change-opacity {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        .animate {
+          animation: reveal-top-to-bottom 1s ease-out forwards, change-opacity 2s ease-out forwards
+        }
+
         .background { fill: #00000000; } 
         .text { fill: #ffffff; font-family: 'Ubuntu', sans-serif; }
         .title { fill: #00f0ff; font-size: 30px; font-weight: bold; }
@@ -15,78 +47,78 @@ function renderStats(stats) {
         .icon { fill: #00f0ff; }
       </style>
       <rect class="background" width="100%" height="100%" />
-      <text x="50" y="50" class="text title">${stats.name}'s GitHub Stats</text>
+      <text x="50" y="50" class="text title animate">${stats.name}'s GitHub Stats</text>
 
-      <g transform="translate(50, 100)">
-        <path class="icon" d="${Icons.commit_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Total Commits:</text>
-        <text x="300" y="0" class="text value">${stats.total_commits}</text>
-      </g>
-
-      <g transform="translate(50, 140)">
-        <path class="icon" d="${Icons.pull_request_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Total PRs:</text>
-        <text x="300" y="0" class="text value">${stats.total_prs}</text>
-      </g>
-
-      <g transform="translate(50, 180)">
+      <g transform="translate(50, 100)" class="animate">
         <path class="icon" d="${Icons.star_icon}" transform="translate(5, -17) scale(0.04)"/>
         <text x="40" y="0" class="text label">Total Stars Earned:</text>
-        <text x="300" y="0" class="text value">${stats.total_stars}</text>
+        <text x="320" y="0" class="text value">${stats.total_stars}</text>
       </g>
 
-      <g transform="translate(50, 220)">
-        <path class="icon" d="${Icons.merged_prs_icon}" transform="translate(5, -17) scale(0.04)"/>
+      <g transform="translate(50, 140)" class="animate">
+        <path class="icon" d="${Icons.contributes_to_icon}" transform="translate(8, -17) scale(0.04)"/>
+        <text x="40" y="0" class="text label">Contributed to:</text>
+        <text x="320" y="0" class="text value">${stats.total_contributes_to}</text>
+      </g>
+
+      <g transform="translate(50, 180)" class="animate">
+        <path class="icon" d="${Icons.followers_icon}" transform="translate(7, -17) scale(0.04)"/>
+        <text x="40" y="0" class="text label">Total Followers:</text>
+        <text x="320" y="0" class="text value">${stats.followers}</text>
+      </g>
+
+      <g transform="translate(50, 220)" class="animate">
+        <path class="icon" d="${Icons.repo_icon}" transform="translate(5, -17) scale(1.4)"/>
+        <text x="40" y="0" class="text label">Total Repos:</text>
+        <text x="320" y="0" class="text value">${stats.total_repos}</text>
+      </g>
+
+      <g transform="translate(50, 260)" class="animate">
+        <path class="icon" d="${Icons.commit_icon}" transform="translate(5, -17) scale(0.04)"/>
+        <text x="40" y="0" class="text label">Total Commits:</text>
+        <text x="320" y="0" class="text value">${stats.total_commits}</text>
+      </g>
+
+      <g transform="translate(50, 300)" class="animate">
+        <path class="icon" d="${Icons.pr_icon}" transform="translate(5, -17) scale(1.4)"/>
+        <text x="40" y="0" class="text label">Total PRs:</text>
+        <text x="320" y="0" class="text value">${stats.total_prs}</text>
+      </g>
+
+      <g transform="translate(50, 340)" class="animate">
+        <path class="icon" d="${Icons.merged_prs_icon}" transform="translate(5, -17) scale(1.4)"/>
         <text x="40" y="0" class="text label">Total PRs Merged:</text>
-        <text x="300" y="0" class="text value">${stats.total_merged_prs}</text>
+        <text x="320" y="0" class="text value">${stats.total_merged_prs}</text>
       </g>
 
-      <g transform="translate(50, 260)">
+      <g transform="translate(50, 380)" class="animate">
+        <path class="icon" d="${Icons.pr_reviewed_icon}" transform="translate(7, -17) scale(0.04)"/>
+        <text x="40" y="0" class="text label">Total PRs Reviewed:</text>
+        <text x="320" y="0" class="text value">${stats.total_prs_reviewed}</text>
+      </g>
+
+      <g transform="translate(50, 420)" class="animate">
         <path class="icon" d="${Icons.merged_prs_percentage_icon}" transform="translate(5, -17) scale(0.04)"/>
         <text x="40" y="0" class="text label">Merged PRs Percentage:</text>
-        <text x="300" y="0" class="text value">${stats.merged_prs_percentage.toFixed(0)}%</text>
+        <text x="320" y="0" class="text value">${stats.merged_prs_percentage.toFixed(0)}%</text>
       </g>
 
-      <g transform="translate(50, 300)">
-        <path class="icon" d="${Icons.pr_reviewed_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Total PRs Reviewed:</text>
-        <text x="300" y="0" class="text value">${stats.total_prs_reviewed}</text>
-      </g>
-
-      <g transform="translate(50, 340)">
-        <path class="icon" d="${Icons.issue_icon}" transform="translate(5, -17) scale(0.04)"/>
+      <g transform="translate(50, 460)" class="animate">
+        <path class="icon" d="${Icons.issue_icon}" transform="translate(5, -18) scale(1.4)"/>
         <text x="40" y="0" class="text label">Total Issues:</text>
-        <text x="300" y="0" class="text value">${stats.total_issues}</text>
+        <text x="320" y="0" class="text value">${stats.total_issues}</text>
       </g>
 
-      <g transform="translate(50, 380)">
-        <path class="icon" d="${Icons.discussions_started_icon}" transform="translate(5, -17) scale(0.04)"/>
+      <g transform="translate(50, 500)" class="animate">
+        <path class="icon" d="${Icons.discussions_started_icon}" transform="translate(5, -18) scale(1.4)"/>
         <text x="40" y="0" class="text label">Total Discussions Started:</text>
-        <text x="300" y="0" class="text value">${stats.total_discussions_started}</text>
+        <text x="320" y="0" class="text value">${stats.total_discussions_started}</text>
       </g>
 
-      <g transform="translate(50, 420)">
-        <path class="icon" d="${Icons.discussions_answered_icon}" transform="translate(5, -17) scale(0.04)"/>
+      <g transform="translate(50, 540)" class="animate">
+        <path class="icon" d="${Icons.discussions_answered_icon}" transform="translate(5, -18) scale(1.4)"/>
         <text x="40" y="0" class="text label">Total Discussions Answered:</text>
-        <text x="300" y="0" class="text value">${stats.total_discussions_answered}</text>
-      </g>
-
-      <g transform="translate(50, 460)">
-        <path class="icon" d="${Icons.followers_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Total Followers:</text>
-        <text x="300" y="0" class="text value">${stats.followers}</text>
-      </g>
-
-      <g transform="translate(50, 500)">
-        <path class="icon" d="${Icons.contributes_to_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Contributed to:</text>
-        <text x="300" y="0" class="text value">${stats.total_contributes_to}</text>
-      </g>
-
-      <g transform="translate(50, 540)">
-        <path class="icon" d="${Icons.repo_icon}" transform="translate(5, -17) scale(0.04)"/>
-        <text x="40" y="0" class="text label">Total Repos:</text>
-        <text x="300" y="0" class="text value">${stats.total_repos}</text>
+        <text x="320" y="0" class="text value">${stats.total_discussions_answered}</text>
       </g>
 
       <circle class="circle-bg" cx="650" cy="200" r="50"></circle>
