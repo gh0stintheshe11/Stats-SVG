bd7698d1db6363b5fea9e1cd4dbf741e879bec1b
gh0stintheshe11 <langs.971104@gmail.com>
2024-07-07 01:54:45 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # readme-status
 
 http://localhost:3000/api/github-status?username=gh0stintheshe11
+
 https://github-readme-stats-lite.vercel.app/api/github-status?username=gh0stintheshe11
--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handler from './api/index.js';
+
 const app = express();
 
 app.get('/api/:action', handler); // Setup a route that matches your API's expected URL structure
