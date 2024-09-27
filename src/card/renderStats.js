00ec2621e4770afbeed3a92fe60c2f3c032b4deb
gh0stintheshe11 <langs.971104@gmail.com>
2024-09-27 03:48:20 -0400

    animation update

--- src/card/renderStats.js
+++ src/card/renderStats.js
@@ -407,14 +407,14 @@ async function renderStats(stats) {
         .rank-circle-bg { 
           fill: none; 
           opacity: 0; 
-          animation: change-opacity 0.3s ease-out 1.5s forwards; 
+          animation: change-opacity 0.3s ease-out 0.8s forwards; 
         }
         .rank-circle-progress { 
           fill: none; 
           opacity: 0; 
           animation: 
-            fillProgress 1.5s ease-out 1.7s forwards, 
-            change-opacity 0s 1.7s forwards; 
+            fillProgress 1.5s ease-out 0.8s forwards, 
+            change-opacity 0s 0.8s forwards; 
           stroke-linecap: round; 
         }
         
@@ -584,14 +584,14 @@ async function renderStats(stats) {
 
       <!-- dot change to short dash line moving to left -->
       <line x1="${rankRingConfig.rank_ring_center_x}" y1="${rankRingConfig.rank_ring_center_y}" x2="${rankRingConfig.rank_ring_center_x}" y2="${rankRingConfig.rank_ring_center_y}" stroke="${elementsConfig.icon_color}" stroke-width="4">
-        <animate attributeName="x2" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_center_x - rankRingConfig.rank_ring_radius*1.7}" dur="0.5s" fill="freeze" begin="0.5s" />
-        <animate attributeName="x1" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_left_end}" dur="0.5s" fill="freeze" begin="1s" />
+        <animate attributeName="x2" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_center_x - rankRingConfig.rank_ring_radius*1.7}" dur="0.5s" fill="freeze" begin="0s" />
+        <animate attributeName="x1" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_left_end}" dur="0.3s" fill="freeze" begin="0.5s" />
       </line>
 
       <!-- dot change to short dash line moving to right -->
       <line x1="${rankRingConfig.rank_ring_center_x}" y1="${rankRingConfig.rank_ring_center_y}" x2="${rankRingConfig.rank_ring_center_x}" y2="${rankRingConfig.rank_ring_center_y}" stroke="${elementsConfig.icon_color}" stroke-width="4">
-        <animate attributeName="x2" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_center_x + rankRingConfig.rank_ring_radius*1.7}" dur="0.5s" fill="freeze" begin="0.5s" />
-        <animate attributeName="x1" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_right_end}" dur="0.5" fill="freeze" begin="1s" />
+        <animate attributeName="x2" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_center_x + rankRingConfig.rank_ring_radius*1.7}" dur="0.5s" fill="freeze" begin="0s" />
+        <animate attributeName="x1" from="${rankRingConfig.rank_ring_center_x}" to="${rankRingConfig.rank_ring_right_end}" dur="0.3" fill="freeze" begin="0.5s" />
       </line>
 
       <!-- top left 1/8 circle path -->
