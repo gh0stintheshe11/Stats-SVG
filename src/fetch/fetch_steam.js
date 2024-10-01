632fad8da791a25a4ba52fcba591d71fc5485a68
gh0stintheshe11 <langs.971104@gmail.com>
2024-09-30 21:56:51 -0400

    update

--- src/fetch/fetch_steam.js
+++ src/fetch/fetch_steam.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import axios from 'axios';
 
+const steamCDNBaseUrl = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/';
+
 // Add a simple in-memory cache
 const cache = new Map();
 const CACHE_TTL = 2 * 60 * 1000; // 2 minutes in milliseconds
@@ -10,6 +12,8 @@ const fetchSteamStatus = async (steamID) => {
     const userProfileUrl = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamID}&format=json`;
     const recentGamesUrl = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json`;
     const ownedGamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamID}&format=json&include_played_free_games=true`;
+    const badgesUrl = `https://api.steampowered.com/IPlayerService/GetBadges/v1/?key=${apiKey}&steamid=${steamID}&format=json`;
+    const animatedAvatarUrl = `https://api.steampowered.com/IPlayerService/GetAnimatedAvatar/v1/?key=${apiKey}&steamid=${steamID}&format=json`;
 
     try {
         // Check if we have cached data
@@ -21,10 +25,12 @@ const fetchSteamStatus = async (steamID) => {
         
         console.time('steam API calls');
         // Fetch data concurrently using Promise.all
-        const [userProfileResponse, recentGamesResponse, ownedGamesResponse] = await Promise.all([
+        const [userProfileResponse, recentGamesResponse, ownedGamesResponse, badgesResponse, animatedAvatarResponse] = await Promise.all([
             axios.get(userProfileUrl),
             axios.get(recentGamesUrl),
-            axios.get(ownedGamesUrl)
+            axios.get(ownedGamesUrl),
+            axios.get(badgesUrl),
+            axios.get(animatedAvatarUrl)
         ]);
         console.timeEnd('steam API calls');
 
@@ -33,13 +39,23 @@ const fetchSteamStatus = async (steamID) => {
         const userProfileData = userProfileResponse.data.response.players[0];
         const recentGamesData = recentGamesResponse.data.response.games || [];
         const ownedGamesData = ownedGamesResponse.data.response.games || [];
+        const animatedAvatarData = animatedAvatarResponse.data.response.avatar || '';
 
         const steamData = {
             username: userProfileData.personaname,
-            avatar: userProfileData.avatarfull,
+            avatar: `${steamCDNBaseUrl}${animatedAvatarData.image_small}`,
             profile_url: userProfileData.profileurl,
+            user_status: userProfileData.personastate === 0 ? 'Offline' :
+                         userProfileData.personastate === 1 ? 'Online' :
+                         userProfileData.personastate === 2 ? 'Busy' :
+                         userProfileData.personastate === 3 ? 'Away' :
+                         userProfileData.personastate === 4 ? 'Snooze' :
+                         userProfileData.personastate === 5 ? 'Looking to trade' :
+                         userProfileData.personastate === 6 ? 'Looking to play' :
+                         'Unknown',
             last_logoff: userProfileData.lastlogoff,
             created: userProfileData.timecreated,
+            steam_level: badgesResponse.data.response.player_level,
             recent_played_games: recentGamesData,
             recent_played_games_count: recentGamesResponse.data.response.total_count,
             total_owned_games: ownedGamesResponse.data.response.game_count,
